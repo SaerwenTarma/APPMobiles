1a19506d8e7338451dda0bb369d59124f560ce0a
SaerwenTarma <69652943+SaerwenTarma@users.noreply.github.com>
2020-09-02 17:44:52 -0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,7 +1,9 @@
+function aprobo(notas) {
+    return notas.every(i => i >= 4);
+}
 
-var notas=[[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]];
+function quienesAprobaron(numeros) {
+    return numeros.filter(i => aprobo(i));
+}
 
-
-var quienesAprobaron = notas.filter(i => i.every(i => i>=4));
-
-console.log(quienesAprobaron);
+console.log(quienesAprobaron([[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]]));
