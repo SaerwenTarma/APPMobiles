ef6a6b1b8b92f35800213c79900064f15c31d58f
SaerwenTarma <69652943+SaerwenTarma@users.noreply.github.com>
2020-09-29 19:27:40 -0300

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,10 +1,11 @@
-function cuantosCumplen (cuantoFiltro, notas)  {
-    return notas.filter(i => cuantoFiltro(i));
+
+function rechazar (Filtro, notas)  {
+    console.log (notas.filter(Filtro));
 };
 
 function even (a){
-return a % 2 != 0;
+    return a % 2 != 0;
+    
+    };
 
-};
-
-console.log(cuantosCumplen(even,[7,9,87,42]));
+   rechazar(even,[7,9,87,42]); 
