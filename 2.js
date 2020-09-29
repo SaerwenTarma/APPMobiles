0fa21d8c1e511059a03a56084bba5583bb322585
SaerwenTarma <69652943+SaerwenTarma@users.noreply.github.com>
2020-09-29 12:52:23 -0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,3 +1,10 @@
-var array=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 
-console.log(array);
+
+function meses(array)
+{
+array.forEach(element => console.log(element));
+
+
+}
+
+console.log("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
